5bc1c6e3d12b5f4540707de3fddcf02b9cb6a556
Deborah Loring <deborah.loring@austin.utexas.edu>
2024-04-09 16:00:19 -0500

    Completed section 6

--- notes/models.py
+++ notes/models.py
@@ -4,3 +4,4 @@ class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,15 @@
+from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
-
+from .forms import NotesForm
 from .models import Notes
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesListView(ListView):
     model = Notes
