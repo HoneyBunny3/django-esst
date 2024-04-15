6af926c5e50a5c643802a6975bdd24bedad73ad9
Deborah Loring <deborah.loring@austin.utexas.edu>
2024-04-15 15:46:05 -0500

    Completed Section 4 (TRAINING DONE!)

--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -27,16 +27,27 @@ def test_list_endpoint_return_user_notes(client, logged_user):
 
 @pytest.mark.django_db
 def test_list_endpoint_only_list_notes_from_authenticated_user(client, logged_user):
-    jon = User.objects.create_user('Jon', 'test2@test.com', 'password')
-    Notes.objects.create(title="Jon's notes", text="", user=jon)
+    another_user_note = NoteFactory()
     
-    note = Notes.objects.create(title='An interesting title', text='', user=logged_user)
-    note2 = Notes.objects.create(title='Another title', text='', user=logged_user)
+    note = NoteFactory(user=logged_user)
+    second_note = NoteFactory(user=logged_user)
     
     response = client.get(path='/smart/notes')
-    assert 200 == response.status_code
     content = str(response.content)
-    assert "An interesting title" in content
-    assert "Another title" in content
-    assert "Jon's note" not in content
+    
+    assert 200 == response.status_code
+    assert note.title in content
+    assert second_note.title in content
+    assert another_user_note.title not in content
     assert 2 == content.count('<h3>')
+    
+@pytest.mark.django_db
+def test_create_endpoint_receives_form_data(client, logged_user):
+    form_data = {'title': 'An impressive title', 'text': 'A really interesting text'}
+    
+    response = client.post(path='/smart/notes/new', data=form_data, follow=True)
+    
+    assert 200 == response.status_code
+    assert 'notes/notes_list.html' in response.template_name
+    assert 1 == logged_user.notes.count()
+    assert "An impressive title" == logged_user.notes.first().title
