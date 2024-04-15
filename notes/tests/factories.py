49609d50ef87757de186841aec6f82372924ee38
Deborah Loring <deborah.loring@austin.utexas.edu>
2024-04-15 15:34:52 -0500

    Completed Section 3

---
+++ notes/tests/factories.py
@@ -0,0 +1,23 @@
+import factory
+from factory import fuzzy
+
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+from notes.models import Notes
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        
+    username = factory.Sequence(lambda n: f"user_{n:04}") # user_0000 user_0001 ... creates user names in a sequence by user_xxxx
+    email = factory.LazyAttribute(lambda user: f"{user.username}@example.com")
+    password = factory.LazyFunction(lambda: make_password('password'))
+    
+class NoteFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Notes
+        
+    title = fuzzy.FuzzyText(length=20)
+    text = fuzzy.FuzzyText(length=200)
+    user = factory.SubFactory(UserFactory)
--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -3,32 +3,35 @@ import pytest
 from django.contrib.auth.models import User
 
 from notes.models import Notes
+from .factories import UserFactory, NoteFactory
+
+@pytest.fixture
+def logged_user(client):
+    user = UserFactory()
+    client.login(username=user.username, password='password')
+    return user
 
 @pytest.mark.django_db
-def test_list_endpoint_return_user_notes(client):
-    user = User.objects.create_user('Hearthy', 'test@test.com', 'password')
-    client.login(username=user.username, password='password')
-    
-    note = Notes.objects.create(title='An interesting title', text='', user=user)
-    note2 = Notes.objects.create(title='Another title', text='', user=user)
+def test_list_endpoint_return_user_notes(client, logged_user):
+        
+    note = NoteFactory(user=logged_user)
+    second_note = NoteFactory(user=logged_user)
     
     response = client.get(path='/smart/notes')
-    assert 200 == response.status_code
     content = str(response.content)
-    assert "An interesting title" in content
-    assert "Another title" in content
+    
+    assert 200 == response.status_code
+    assert note.title in content
+    assert second_note.title in content
     assert 2 == content.count('<h3>')
 
 @pytest.mark.django_db
-def test_list_endpoint_only_list_notes_from_authenticated_user(client):
+def test_list_endpoint_only_list_notes_from_authenticated_user(client, logged_user):
     jon = User.objects.create_user('Jon', 'test2@test.com', 'password')
     Notes.objects.create(title="Jon's notes", text="", user=jon)
     
-    user = User.objects.create_user('Hearthy', 'test@test.com', 'password')
-    client.login(username=user.username, password='password')
-    
-    note = Notes.objects.create(title='An interesting title', text='', user=user)
-    note2 = Notes.objects.create(title='Another title', text='', user=user)
+    note = Notes.objects.create(title='An interesting title', text='', user=logged_user)
+    note2 = Notes.objects.create(title='Another title', text='', user=logged_user)
     
     response = client.get(path='/smart/notes')
     assert 200 == response.status_code
